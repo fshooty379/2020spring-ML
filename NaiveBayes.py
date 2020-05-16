9c4a6cdf937b62bbf4d32371cd61b2dfc2ef2b1a
fshooty <594626385@qq.com>
2020-05-16 20:06:00 +0800

    laplace

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -9,14 +9,14 @@ from sklearn.metrics import accuracy_score, f1_score
 from sklearn.datasets import fetch_20newsgroups
 
 # 挑选部分数据集来进行bug测试
-# categories = ['alt.atheism', 'comp.graphics', 'comp.os.ms-windows.misc',
-#              'talk.religion.misc', 'sci.electronics']
+categories = ['alt.atheism', 'comp.graphics', 'comp.os.ms-windows.misc',
+              'talk.religion.misc', 'sci.electronics']
 # 'rec.sport.baseball', 'sci.space', 'talk.politics.guns', 'sci.med']
 # 训练集
-# newsgroups_train = fetch_20newsgroups(subset='train', categories=categories, remove='header')
+#newsgroups_train = fetch_20newsgroups(subset='train', categories=categories, remove='header')
 newsgroups_train = fetch_20newsgroups(subset='train', remove='header')
 # 测试集
-# newsgroups_test = fetch_20newsgroups(subset='test',categories=categories,remove='header')
+#newsgroups_test = fetch_20newsgroups(subset='test',categories=categories,remove='header')
 newsgroups_test = fetch_20newsgroups(subset='test', remove='header')
 
 # 导入本地停用词表
@@ -26,7 +26,7 @@ with open("D:\大二下_课程资料\机器学习实验\skl\stopwords.txt", "rb"
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # 停用词为stopwords.txt，全部转换为小写，选择词频为前3000的作为特征，构造稀疏矩阵
-vectorizer = TfidfVectorizer(stop_words=stpwrdlst, lowercase=True, max_features=3000)
+vectorizer = TfidfVectorizer(stop_words=stpwrdlst, lowercase=True, max_features=2000)
 # 训练集对应稀疏矩阵
 vectors_train = vectorizer.fit_transform(newsgroups_train.data)
 # 测试集对应稀疏矩阵
@@ -47,10 +47,11 @@ test_y = newsgroups_test.target  # 测试集标签
 
 
 class NaiveBayes:
-    def __init__(self, num_class):
+    def __init__(self, num_class, alpha):
         self.classes = num_class  # 保存分类的种类数
         self.prior = np.zeros(num_class)  # 存储P(Y)
         self.Pxy = np.zeros((num_class, 20000))  # 存储后验概率P(X|Y)
+        self.alpha = alpha
 
     # 求每篇文档属于哪一个类的先验概率P（Y）
     def calculate_prior(self, data):
@@ -71,7 +72,7 @@ class NaiveBayes:
         pickle.dump(self.prior, open('NB_prior.pkl', 'wb'))
         # 然后计算在每一个类别Y下每一个属性X的后验概率
         for i in range(self.classes):
-            m = []  # 属于该类的样本
+            m = []  # 存储属于该类的样本
             sum_m = 0.
             sum_c = 0  # 属于该类的样本
             for j in range(y.shape[0]):
@@ -84,11 +85,11 @@ class NaiveBayes:
                     m.append(temp)
                     sum_m += np.sum(temp)
             print('属于类别' + str(i) + '的有' + str(sum_c) + '个')
-            m = np.array(m).T  # 将样本矩阵转置
+            m = np.array(m).T  #转置
             for j in range(x.shape[1]):  # 遍历所有特征
                 sum = np.sum(m[j])
-                # 求出条件概率pxy
-                self.Pxy[i][j] = (100000.0) * (sum + 1) / (sum_m + x.shape[1])
+                # 贝叶斯估计求出条件概率pxy,加入拉普拉斯平滑
+                self.Pxy[i][j] = (100000.0) * (sum + self.alpha) / (sum_m + x.shape[1] * self.alpha)
 
     def predict(self, x):
         print('start predicting')
@@ -108,10 +109,11 @@ class NaiveBayes:
         return pred_labels
 
 
+from sklearn.metrics import classification_report
 if __name__ == "__main__":
     classes = np.unique(train_y)  # 去掉多余项，得到分类的种类数
     class_nums = len(classes)
-    model = NaiveBayes(num_class=class_nums)
+    model = NaiveBayes(num_class=class_nums,alpha=1)
     # 训练模型
     model.train(train_x, train_y)
 
@@ -126,4 +128,4 @@ if __name__ == "__main__":
             sum = sum + 1
     accuracy = sum / float(test_x.shape[0])
     print('NaiveBayes_accuracy = ', accuracy)
-
+    print(classification_report(test_y, pred, target_names=newsgroups_test.target_names))
